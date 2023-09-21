6177cf3aa5567d65207adfdd804ee39ca4d2d488
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2023-09-21 09:30:22 +0200

    init

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { TouchableOpacity, StyleSheet, Text, View, Alert } from 'react-native';
+import PhoneInput from 'react-native-phone-number-input';
 
-export default function App() {
+const App = () => {
+const [phoneNumber, setPhoneNumber] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Phone Number Input</Text>
+
+      <PhoneInput
+      defaultValue={phoneNumber}
+      defaultCode='ZA'
+      withShadow
+      onChangeFormattedText={text => setPhoneNumber(text)}
+      />
+
+      <View style={styles.btnContainer} >
+      <TouchableOpacity style={styles.acceptButton} onPress={() => Alert.alert(phoneNumber)} />
+
+      <TouchableOpacity style={styles.rejectButton} onPress={() => Alert.alert("Why?? Araba phone!!")} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +36,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btnContainer: {
+    marginTop: 30,
+    maxWidth: 300,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  acceptButton: {
+    width: 120,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: "#00cc00",
+    marginRight: 10
+  },
+  rejectButton: {
+    width: 120,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: "#e60000"
+  }
 });
